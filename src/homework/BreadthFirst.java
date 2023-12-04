fb9cb19e1ab31d807ecccdbfc2700a077190ef99
Fionn <psychedover@gmail.om>
2023-12-04 02:01:43 -0800

    BreadthFirstSearch working

--- src/homework/BreadthFirst.java
+++ src/homework/BreadthFirst.java
@@ -1,20 +1,30 @@
 package homework;
 
-import edu.princeton.cs.algs4.Graph;
-import edu.princeton.cs.algs4.In;
-import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.*;
 
 public class BreadthFirst {
     public static void main(String[] unused) {
-        In file = new In("ddeg.txt");
+        In file = new In("Breadth.txt");
         int v = file.readInt();
         int e = file.readInt();
-        Graph g = new Graph(v + 1);
+        Digraph g = new Digraph(v + 1);
 
         for (int i = 0; i < e; i++) {
             int v1 = file.readInt();
             int v2 = file.readInt();
             g.addEdge(v1, v2);
         }
+
+        BreadthFirstDirectedPaths dist = new BreadthFirstDirectedPaths(g, 1) ;
+
+        for (int i = 1; i <= v; i++) {
+            if (dist.hasPathTo(i)) {
+                StdOut.print(dist.distTo(i)) ;
+            } else {
+                StdOut.print("-1");
+            }
+            StdOut.print(" ");
+        }
+        StdOut.println() ;
     }
 }
