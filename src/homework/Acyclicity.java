30b2ca19f7df02b8583496366211a22cd0477ca1
Fionn <psychedover@gmail.com>
2023-12-04 13:20:52 -0800

    Acyclicity and Topological working :)

--- src/homework/Acyclicity.java
+++ src/homework/Acyclicity.java
@@ -5,13 +5,13 @@ import edu.princeton.cs.algs4.* ;
 public class Acyclicity {
     public static void main(String[] unused) {
         In file = new In("Acyclicity.txt") ;
-        int num = file.readInt();
+        int num = file.readInt() ;
 
         // Creates graph
-        for (int i = 0; i <= num; i++) {
+        for (int i = 0; i < num; i++) {
             int v = file.readInt() ;
             int e = file.readInt() ;
-            Graph g = new Graph(v + 1) ;
+            Digraph g = new Digraph(v + 1) ;
 
             // Adds edges to graph
             for (int j = 0; j < e; j++) {
@@ -19,6 +19,12 @@ public class Acyclicity {
                 int v2 = file.readInt() ;
                 g.addEdge(v1, v2) ;
             }
+
+            DirectedCycle bicycle = new DirectedCycle(g) ;
+            if(bicycle.hasCycle()) {
+                StdOut.print("-1") ;
+            } else { StdOut.print("1") ; }
+            StdOut.print(" ") ;
         }
     }
 }
